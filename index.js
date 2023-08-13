ea42bec9d0a0f398879d2c5ecf23570f279417b7
Pisut Sritrakulchai <pudkrong@hotmail.com>
2023-08-14 00:40:01 +0200

    feat: add hook

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const A = require("async");
 const mqtt = require("mqtt");
 const debug = require("debug")("engine:mqtt");
-const { faker } = require("@faker-js/faker");
+const engineUtil = require("@artilleryio/int-commons").engine_util;
+const ensurePropertyIsAList = engineUtil.ensurePropertyIsAList;
+const _ = require("lodash");
 
 const _get = (object, path) => {
   const keys = path.split(".");
@@ -46,7 +48,40 @@ class MqttEngine {
   // to create a VU function
   createScenario(scenarioSpec, ee) {
     const self = this;
-    const tasks = scenarioSpec.flow.map((rs) => this.step(rs, ee));
+
+    ensurePropertyIsAList(scenarioSpec, "beforeScenario");
+    ensurePropertyIsAList(scenarioSpec, "afterScenario");
+    ensurePropertyIsAList(scenarioSpec, "beforeRequest");
+
+    // Add scenario-level hooks if needed:
+    // For now, just turn them into function steps and insert them
+    // directly into the flow array.
+    // TODO: Scenario-level hooks will probably want access to the
+    // entire scenario spec rather than just the userContext.
+    const beforeScenarioFns = _.map(
+      scenarioSpec.beforeScenario,
+      function (hookFunctionName) {
+        return { function: hookFunctionName };
+      }
+    );
+    const afterScenarioFns = _.map(
+      scenarioSpec.afterScenario,
+      function (hookFunctionName) {
+        return { function: hookFunctionName };
+      }
+    );
+
+    const newFlow = beforeScenarioFns.concat(
+      scenarioSpec.flow.concat(afterScenarioFns)
+    );
+
+    scenarioSpec.flow = newFlow;
+
+    const tasks = scenarioSpec.flow.map((rs) =>
+      this.step(rs, ee, {
+        beforeRequest: scenarioSpec.beforeRequest,
+      })
+    );
 
     return function scenario(initialContext, callback) {
       ee.emit("started");
@@ -57,6 +92,7 @@ class MqttEngine {
           self.config.auth,
           initialContext
         );
+        debug("mqtt config", parsedAuth);
         const { auth, ...rest } = self.config;
         debug("mqtt is connecting as", parsedAuth.clientId);
         const mqttClient = mqtt.connect(self.target, {
@@ -122,11 +158,12 @@ class MqttEngine {
 
   // This is a convenience function where we delegate common actions like loop, log, and think,
   // and handle actions which are custom for our engine, i.e. the "doSomething" action in this case
-  step(rs, ee) {
+  step(rs, ee, opts) {
+    opts = opts || {};
     const self = this;
 
     if (rs.loop) {
-      const steps = rs.loop.map((loopStep) => this.step(loopStep, ee));
+      const steps = rs.loop.map((loopStep) => this.step(loopStep, ee, opts));
 
       return this.helpers.createLoopWithCount(rs.count || -1, steps, {
         loopValue: rs.loopValue || "$loopCount",
@@ -152,7 +189,7 @@ class MqttEngine {
 
     if (rs.function) {
       return function (context, callback) {
-        let func = self.script.config.processor[rs.function];
+        const func = self.script.config.processor[rs.function];
         if (!func) {
           return process.nextTick(function () {
             callback(null, context);
@@ -168,53 +205,69 @@ class MqttEngine {
     //
     // This is our custom action:
     //
-    // TODO::PUD implement publish function
     if (rs.publish) {
-      return function publish(context, callback) {
+      function f(context, callback) {
         const topic = self.helpers.template(rs.publish.topic, context);
         const payload = self.helpers.template(rs.publish.payload, context);
 
-        debug("publish topic", topic);
-        debug("publish payload", payload);
-        context.mqtt.publish(
-          topic,
-          typeof payload === "string" ? payload : JSON.stringify(payload),
-          rs.publish.options,
-          (error) => {
-            if (error) {
-              ee.emit("error", error.message || error.code);
-              return callback(error);
+        // Custom action: publish
+        function publish(context, callback) {
+          debug("publish topic", topic);
+          debug("publish payload", payload);
+          context.mqtt.publish(
+            topic,
+            typeof payload === "string" ? payload : JSON.stringify(payload),
+            rs.publish.options,
+            (error) => {
+              if (error) {
+                ee.emit("error", error.message || error.code);
+                return callback(error);
+              }
+
+              ee.emit("counter", "mqtt.publish_count", 1);
+              ee.emit("rate", "mqtt.publish_rate");
+              return callback(null, context);
+            }
+          );
+        }
+
+        const functionNames = _.concat(
+          opts.beforeRequest || [],
+          rs.publish.beforeRequest || []
+        );
+
+        A.eachSeries(
+          functionNames,
+          function iteratee(functionName, next) {
+            const fn = self.helpers.template(functionName, context);
+            const processFunc = self.script.config.processor[fn];
+            if (!processFunc) {
+              processFunc = function (r, c, e, cb) {
+                return cb(null);
+              };
+              console.log(`WARNING: custom function ${fn} could not be found`); // TODO: a 'warning' event
             }
 
-            ee.emit("counter", "mqtt.publish_count", 1);
-            ee.emit("rate", "mqtt.publish_rate");
-            return callback(null, context);
+            processFunc({ topic, payload }, context, ee, function (err) {
+              if (err) {
+                return next(err);
+              }
+              return next(null);
+            });
+          },
+          function done(err) {
+            if (err) {
+              debug(err);
+              return callback(err, context);
+            }
+
+            // Finally call the request spec function
+            return publish(context, callback);
           }
         );
-      };
-    }
+      }
 
-    if (rs.faker) {
-      return function (context, callback) {
-        const name = rs.faker.name;
-        const args = rs.faker.parameters;
-        const variable = rs.faker.store;
-
-        // TONOTE:: Hack using timestamp
-        if (name === "timestamp") {
-          context.vars[variable] = Date.now();
-          return callback(null, context);
-        } else {
-          const func = _get(faker, name);
-          if (!func) {
-            return process.nextTick(function () {
-              callback(null, context);
-            });
-          }
-          context.vars[variable] = args ? func.call(func, ...args) : func();
-          return callback(null, context);
-        }
-      };
+      return f;
     }
 
     //
